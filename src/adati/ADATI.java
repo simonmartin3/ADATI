e242130a8da5c45a00ea4c881fd1c35f056e5738
Simon MARTIN <simon.martin3@student.hepl.be>
2020-09-19 23:17:35 +0200

    update 28/05

---
+++ src/adati/ADATI.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adati;
+
+/**
+ *
+ * @author Simon
+ */
+public class ADATI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        windowMain WM = new windowMain();
+        WM.setVisible(true);
+        
+    }
+    
+}
